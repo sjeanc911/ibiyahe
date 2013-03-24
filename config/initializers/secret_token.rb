764e2e54ad80a7ea256839305316a9b242f33e1e
jean <sjeanc911@gmail.com>
2013-03-24 17:40:33 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ibiyahe::Application.config.secret_token = '06ddd35b50561b75c4895d36d781794fee145dd29c5469987bf397bf794a1e722669972764695ae1cfa8216b9de1d4211f8736baa8c6278366e8b5584e5456e5'
